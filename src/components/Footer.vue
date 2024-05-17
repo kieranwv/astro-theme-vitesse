573ade066d429a1be43b38ee5dd83d90a06ea533
kaivanwong <kaivanwong@outlook.com>
2024-05-17 11:01:51 +0800

    fix: fix footer color

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -4,7 +4,7 @@ import { getLinkTarget } from '@/utils/link'
 </script>
 
 <template>
-  <footer class="w-full mt-18 pt-6 pb-8 max-w-3xl text-sm flex flex-col gap-4 border-main border-t !border-op-50">
+  <footer class="w-full mt-18 pt-6 pb-8 max-w-3xl text-sm flex flex-col gap-4 border-main border-t !border-op-50 text-dark dark:text-white">
     <div v-if="siteConfig.footer.navLinks && siteConfig.footer.navLinks.length > 0" class="flex flex-wrap gap-4">
       <template v-for="(link, index) in siteConfig.footer.navLinks" :key="link.text">
         <a
@@ -13,10 +13,10 @@ import { getLinkTarget } from '@/utils/link'
         >
           {{ link.text }}
         </a>
-        <span v-if="index < siteConfig.footer.navLinks.length - 1"> / </span>
+        <span v-if="index < siteConfig.footer.navLinks.length - 1" op-70> / </span>
       </template>
     </div>
-    <div class="flex text-dark dark:text-white">
+    <div flex>
       <a nav-link href="https://creativecommons.org/licenses/by-nc-sa/4.0/" target="_blank">CC BY-NC-SA 4.0</a>
       <span op-70>&nbsp;&nbsp;&copy;&nbsp;&nbsp;{{ new Date().getFullYear() }}&nbsp;&nbsp;{{ siteConfig.author
       }}.</span>
