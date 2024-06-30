3ef3c2eed1549953665e383aff1a97e1ed6dd59f
kevinwong <kevinwong865@gmail.com>
2024-06-30 15:50:21 +0800

    feat: add theme toggle transition

--- src/components/ThemeToggle.vue
+++ src/components/ThemeToggle.vue
@@ -4,8 +4,46 @@ import { useDark, useToggle } from '@vueuse/core'
 const isDark = useDark()
 
 const toggleDark = useToggle(isDark)
+
+function toggleTheme(event: MouseEvent) {
+  const x = event.clientX
+  const y = event.clientY
+  const endRadius = Math.hypot(
+    Math.max(x, innerWidth - x),
+    Math.max(y, innerHeight - y),
+  )
+  // @ts-expect-error: Transition API
+  if (!document.startViewTransition) {
+    toggleDark()
+    return
+  }
+
+  // @ts-expect-error: Transition API
+  const transition = document.startViewTransition(async () => {
+    toggleDark()
+  })
+
+  transition.ready.then(() => {
+    const clipPath = [
+      `circle(0px at ${x}px ${y}px)`,
+      `circle(${endRadius}px at ${x}px ${y}px)`,
+    ]
+    document.documentElement.animate(
+      {
+        clipPath: isDark.value ? [...clipPath].reverse() : clipPath,
+      },
+      {
+        duration: 400,
+        easing: 'ease-in',
+        pseudoElement: isDark.value
+          ? '::view-transition-old(root)'
+          : '::view-transition-new(root)',
+      },
+    )
+  })
+}
 </script>
 
 <template>
-  <button :aria-label="isDark ? 'Dark Theme' : 'Light Theme'" nav-link dark:i-ri-moon-line i-ri-sun-line @click="toggleDark()" />
+  <button :aria-label="isDark ? 'Dark Theme' : 'Light Theme'" nav-link dark:i-ri-moon-line i-ri-sun-line @click="toggleTheme" />
 </template>
